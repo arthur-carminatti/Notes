100df81e5a86768f19d2dc93b5144836aa5529b9
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-08-05 19:48:56 -0300

    User image

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,19 @@
-import { Container } from "./styles";
+import { Container, Profile } from "./styles";
 
 export function Header() {
     return (
-        <Container></Container>
+        <Container>
+            <Profile>
+                <img 
+                src="https://github.com/arthur-carminatti.png" 
+                alt="foto do usuário" 
+                />
+
+                <div>
+                    <span>Bem-vindo</span>
+                    <strong>Arthur Carminatti</strong>
+                </div>
+            </Profile>
+        </Container>
     )
 }
