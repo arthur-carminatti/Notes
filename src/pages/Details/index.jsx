1dec543cab5bd054d75843d9153d58ad2956a4dc
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-08-05 00:43:53 -0300

    add button props

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,12 @@
+import { Button } from "../../components/Button/index.jsx"
 import { Container } from "./styles.js"
 
 export function Details() {
   return (
     <Container>
       <h1>Hello World</h1>
+
+      <Button title="Entrar" loading/>
     </Container>
   )
 }
