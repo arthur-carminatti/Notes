55b3aa884c75c561ed4238dd14ec250cb4c72139
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-08-06 15:12:25 -0300

    Finish details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,28 +1,45 @@
 import { Button } from "../../components/Button/index.jsx"
+import { ButtonText } from "../../components/ButtonText/index.jsx"
 import { Header } from "../../components/Header/index.jsx"
 import { Section } from "../../components/Section/index.jsx"
 import { Tag } from "../../components/Tag/index.jsx"
-import { Container, Links } from "./styles.js"
+import { Container, Content, Links } from "./styles.js"
 
 export function Details() {
   return (
     <Container>
       <Header />
 
-      <Section title="Links úteis" >
-        <Links>
-          <li>
-            <a href="https://github.com/arthur-carminatti">https://github.com/arthur-carminatti</a>
-          </li>
-        </Links>
-      </Section>
+      <main>
+        <Content>
+          <ButtonText title="Excluir Nota" />
 
-      <Section title="Marcadores" >
-        <Tag title="express" />
-        <Tag title="nodejs" />
-      </Section>
+          <h1>
+            Introdução ao React
+          </h1>
 
-      <Button title="Entrar" loading />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, dolores reiciendis similique natus nemo minima deserunt nihil?
+            Excepturi, sit tempora iure provident reiciendis, iste ipsum ipsam perspiciatis dolores sint rerum.
+          </p>
+
+          <Section title="Links úteis" >
+            <Links>
+              <li>
+                <a href="https://github.com/arthur-carminatti">https://github.com/arthur-carminatti</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores" >
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+
+          <Button title="Entrar" loading />
+
+        </Content>
+      </main>
     </Container>
   )
 }
