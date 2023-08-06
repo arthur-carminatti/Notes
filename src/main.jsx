f7624b04cb0ad31687c4c482e4ad475845fc7e0a
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-08-06 17:26:02 -0300

    Add grid in home page

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import { Details } from './pages/Details'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
+import { Home } from './pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,30 @@
+import { Header } from "../../components/Header";
+import { Brand, Container, Content, Menu, Search, NewNote } from "./styles";
+
+export function Home() {
+    return (
+        <Container>
+            <Brand>
+                <h1>Notes</h1>
+            </Brand>
+
+            <Header />
+
+            <Menu>
+
+            </Menu>
+
+            <Search>
+
+            </Search>
+
+            <Content>
+
+            </Content>
+
+            <NewNote>
+
+            </NewNote>
+        </Container>
+    )
+}
