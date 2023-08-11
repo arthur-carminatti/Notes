7a74c5f2551a84f2a6217a28032f1655b0b254af
Arthur Carminatti <arthurcarminattigit@gmail.com>
2023-08-10 22:23:11 -0300

    Create profile

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,13 @@ import GlobalStyles from './styles/global'
 import { Home } from './pages/Home'
 import { SignIn } from './pages/SignIn'
 import { SignUp } from './pages/SignUp'
+import { Profile } from './pages/Profile'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignUp />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>,
 )
